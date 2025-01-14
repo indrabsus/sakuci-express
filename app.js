4f42e9c5605331189ec94a0f217619bd3ee708b8
indrabsus <bataravix3r@gmail.com>
2025-01-14 21:36:36 +0700

    cik ah

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require('dotenv').config();
 const authRoutes = require('./routes/authRoutes');
-// const protectedRoutes = require('./routes/protectedRoutes');
+const protectedRoutes = require('./routes/protectedRoutes');
 const ppdbRoutes = require('./routes/ppdbRoutes');
 
 const client = require('./wabot');
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 
 
-// app.use('/api', protectedRoutes);
+app.use('/api', protectedRoutes);
 
 app.use('/ppdb', ppdbRoutes)
 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,13 +1,25 @@
 // authRoutes.js
-require('dotenv').config();
 const express = require('express');
-// const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
+require('dotenv').config();
 
 // Endpoint untuk login dan menghasilkan JWT
 router.post('/login', (req, res) => {
   const { username, password } = req.body;
 
+  // Proses validasi username dan password (misalnya, cek di database)
+  if (username === 'user' && password === 'password123') {
+    const payload = { userId: 1, username }; // Data yang ingin Anda masukkan dalam token
+    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' }); // Membuat token dengan expire 1 jam
+
+    return res.status(200).json({
+      message: 'Login Successful!',
+      token: token, // Kirimkan token JWT ke client
+    });
+  } else {
+    return res.status(401).json({ message: 'Invalid Credentials' });
+  }
 });
 
 module.exports = router;
