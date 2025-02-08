b803846a6605a2e70f768bdbfac50271e5f18a43
indrabsus <bataravix3r@gmail.com>
2025-02-08 16:39:51 +0700

    gaskeun

--- routes/ppdbRoutes.js
+++ routes/ppdbRoutes.js
@@ -1,14 +1,15 @@
 const express = require('express');
+const proteksi = require('../middleware/authMiddleware');
 const { dataSiswa, regisSiswa, jurusan, bayarDaftar, deleteLog, detailSiswa, bayarPpdb, logPpdb } = require('../controllers/ppdbController');
 const router = express.Router();
 
-router.get('/siswa/:tahun/:status?', dataSiswa);
-router.get('/log/:tahun', logPpdb);
-router.get('/detailsiswa/:id_siswa/:tahun', detailSiswa);
+router.get('/siswa/:tahun/:status?', proteksi,  dataSiswa);
+router.get('/log/:tahun', proteksi, logPpdb);
+router.get('/detailsiswa/:id_siswa/:tahun', proteksi, detailSiswa);
 router.post('/daftar', regisSiswa);
 router.get('/jurusan', jurusan);
-router.post('/bayardaftar', bayarDaftar);
-router.post('/bayarppdb', bayarPpdb);
-router.delete('/deletelog', deleteLog);
+router.post('/bayardaftar', proteksi, bayarDaftar);
+router.post('/bayarppdb', proteksi, bayarPpdb);
+router.delete('/deletelog', proteksi, deleteLog);
 
 module.exports = router;
